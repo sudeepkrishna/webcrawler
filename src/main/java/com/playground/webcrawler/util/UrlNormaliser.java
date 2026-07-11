dd10ee0ce4c2fe59e8eb76444ad6962a10c59392
sudeepkrishna <sudeepkn12@gmail.com>
2026-07-11 17:50:39 +0100

    Improve url normaliser to handle default ports and remove fragments, lowercase only schema and host

--- src/main/java/com/playground/webcrawler/util/UrlNormaliser.java
+++ src/main/java/com/playground/webcrawler/util/UrlNormaliser.java
@@ -1,12 +1,31 @@
 package com.playground.webcrawler.util;
 
 import java.net.URI;
-import java.util.Locale;
+import java.net.URISyntaxException;
 
 public class UrlNormaliser {
 
-  public static String normalise(String rawUrl) {
-    URI uri = URI.create(rawUrl);
-    return uri.normalize().toString().toLowerCase(Locale.ROOT);
+  public static String normalise(String originalHost, String relativePath)
+      throws URISyntaxException {
+    URI hostUri = URI.create(originalHost);
+    URI newUrl = hostUri.resolve(relativePath);
+
+    String scheme = newUrl.getScheme().toLowerCase();
+    String host = newUrl.getHost().toLowerCase();
+    int originalPort = newUrl.getPort();
+    String path = newUrl.getPath();
+    String query = newUrl.getQuery();
+
+    int newPort = isDefaultPort(scheme, originalPort) ? -1 : originalPort;
+
+    URI normalisedUri = new URI(scheme, null, host, newPort, path, query, null);
+
+    URI normalisedUriAfterPathCompression = normalisedUri.normalize();
+
+    return normalisedUriAfterPathCompression.toString();
+  }
+
+  private static boolean isDefaultPort(String scheme, int port) {
+    return scheme.equals("http") && port == 80 || scheme.equals("https") && port == 443;
   }
 }
